fea6f9983fd4aa305a3d26fbbcf7ae32cb93cd3b
Kishore Newton <kishorenke@gmail.com>
2023-09-30 18:30:20 +0530

    initial commit

---
+++ latest_nginx.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Constants
+NGINX_GIT_REPO="https://github.com/nginx/nginx.git"
+NGINX_DIR="/tmp/nginx"
+SYSTEMD_SERVICE_FILE="/etc/systemd/system/nginx.service"
+
+# Step 1: Install Required Dependencies
+sudo apt update
+sudo apt install -y build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev openssl libssl-dev git
+
+# Step 2: Clone NGINX Repo
+git clone $NGINX_GIT_REPO $NGINX_DIR
+cd $NGINX_DIR
+
+# Step 3: Configure the Build
+./auto/configure \
+  --prefix=/etc/nginx \
+  --sbin-path=/usr/sbin/nginx \
+  --modules-path=/usr/lib/nginx/modules \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/run/nginx.lock \
+  --http-client-body-temp-path=/var/cache/nginx/client_temp \
+  --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+  --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+  --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+  --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+  --with-compat \
+  --with-file-aio \
+  --with-threads \
+  --with-http_addition_module \
+  --with-http_auth_request_module \
+  --with-http_dav_module \
+  --with-http_flv_module \
+  --with-http_gunzip_module \
+  --with-http_gzip_static_module \
+  --with-http_mp4_module \
+  --with-http_random_index_module \
+  --with-http_realip_module \
+  --with-http_secure_link_module \
+  --with-http_slice_module \
+  --with-http_ssl_module \
+  --with-http_stub_status_module \
+  --with-http_sub_module \
+  --with-http_v2_module \
+  --with-mail \
+  --with-mail_ssl_module \
+  --with-stream \
+  --with-stream_realip_module \
+  --with-stream_ssl_module \
+  --with-stream_ssl_preread_module
+
+# Step 4: Compile and Install
+make
+sudo make install
+
+# Step 5: Create Systemd Service File (Optional)
+echo "[Unit]
+Description=nginx - high performance web server
+Documentation=https://nginx.org/en/docs/
+After=network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t -c /etc/nginx/nginx.conf
+ExecStart=/usr/sbin/nginx -c /etc/nginx/nginx.conf
+ExecReload=/bin/kill -s HUP \$MAINPID
+ExecStop=/bin/kill -s TERM \$MAINPID
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee $SYSTEMD_SERVICE_FILE
+
+# Step 6: Enable and Start Service
+sudo mkdir -p /var/cache/nginx/client_temp
+sudo systemctl daemon-reload
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo nginx -t
+
+echo "NGINX installed and running!"
+
